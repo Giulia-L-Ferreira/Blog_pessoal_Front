56dd2f4e10049be90e75b4772c6081520515abba
Giulia-L-Ferreira <lopesgiulia30@gmail.com>
2025-06-08 03:14:09 -0300

    atualizações para deploy

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,17 @@ import FormTema from './components/temas/formtema/FormTema';
 import DeletarTema from './components/temas/deletartema/DeletarTema';
 import ListaPostagens from './components/postagens/listapostagens/ListaPostagens';
 import FormPostagem from './components/postagens/formpostagem/FormPostagem';
+import DeletarPostagem from './components/temas/deletarpostagem/DeletarPostagem';
+import Perfil from './pages/perfil/Perfil';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 
 function App() {
 
   return (
     <AuthProvider>
+      <ToastContainer />
       <BrowserRouter>
         <Navbar />
         <div className='min-h-[80vh]'>
@@ -31,6 +36,8 @@ function App() {
             <Route path="/postagens" element={<ListaPostagens />} />
             <Route path="/cadastrarpostagem" element={<FormPostagem />} />
             <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+            <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+            <Route path="/perfil" element={<Perfil />} />
           </Routes>
         </div>
       </BrowserRouter>
--- src/components/temas/cardtemas/CardTemas.tsx
+++ src/components/temas/cardtemas/CardTemas.tsx
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom'
 import type Tema from '../../../models/Tema'
 
-interface CardTemasProps {
+
+interface CardTemasProps{
     tema: Tema
 }
 
@@ -12,7 +13,7 @@ function CardTemas({ tema }: CardTemasProps) {
                 Tema
             </header>
             <p className='p-8 text-3xl bg-slate-200 h-full'>{tema.descricao}</p>
-
+            
             <div className="flex">
                 <Link to=''
                     className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800 
@@ -20,15 +21,8 @@ function CardTemas({ tema }: CardTemasProps) {
                     <button>Editar</button>
                 </Link>
 
-                <Link to={`/editartema/${tema.id}`}
-                    className='w-full text-slate-100 bg-	indigo-400 hover:bg-indigo-800 
-    flex items-center justify-center py-2'>
-                    <button>Editar</button>
-                </Link>
-
-                <Link to={`/deletartema/${tema.id}`}
-                    className='text-slate-100 bg-red-400 hover:bg-red-700 w-full 
-		flex items-center justify-center'>
+                <Link to='' className='text-slate-100 bg-red-400 hover:bg-red-700 w-full 
+                    flex items-center justify-center'>
                     <button>Deletar</button>
                 </Link>
             </div>
