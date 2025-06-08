b22f8aa0af753205f7f85f9ac4bb12479b4dfd74
Giulia-L-Ferreira <lopesgiulia30@gmail.com>
2025-06-08 01:58:47 -0300

    criação cadastrar temas

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,31 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
-import { UserProvider } from './context/UserContext';
+import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/temas/listatemas/ListaTemas';
+import Navbar from './components/navbar/Navbar';
+import Cadastro from './pages/cadastro/Cadastro';
+import FormTema from './components/temas/formtema/FormTema';
 
 function App() {
 
   return (
-    <UserProvider>
+    <AuthProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
+        <Navbar />
+        <div className='min-h-[80vh]'>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/temas" element={<ListaTemas />} />
+            <Route path="/cadastrartema" element={<FormTema />} />
+            <Route path="/editartema/:id" element={<FormTema />} />
+          </Routes>
+        </div>
       </BrowserRouter>
-    </UserProvider>
+    </AuthProvider>
   );
 }
 
